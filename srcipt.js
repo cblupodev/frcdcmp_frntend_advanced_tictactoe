0dd6b157986079b7d51b8ba5e535c27d8f61ac34
Christopher <chrisblupodev@gmail.com>
2016-06-03 13:33:12 +0000

    bring over from codepen

--- srcipt.js
+++ srcipt.js
@@ -1,5 +1,5 @@
-function l (message) {
-  console.log(message);
+function l(message) {
+    console.log(message);
 }
 
 // '{0}{1}'.lp_format('asdf', 1 + 2);
@@ -24,9 +24,9 @@ if (!Array.lp_allEqualTo) {
     Array.prototype.lp_allEqualTo = function(elem, comparator) {
         var rtn = true;
         this.forEach(function(e) {
-           if (e !== elem) {
-               rtn = false;
-           } 
+            if (e !== elem) {
+                rtn = false;
+            }
         });
         return rtn;
     }
@@ -38,21 +38,23 @@ $(document).ready(function() {
 
     var boxes = lp_initfillArray(-1, 9);
     var $boxes = $('table button');
-    
+
     function checkWinners() {
-        var allElements = [[boxes[0], boxes[1], boxes[2]],
-                           [boxes[3], boxes[4], boxes[5]],
-                           [boxes[6], boxes[7], boxes[8]],
-                           [boxes[0], boxes[3], boxes[6]],
-                           [boxes[1], boxes[4], boxes[7]],
-                           [boxes[2], boxes[5], boxes[8]],
-                           [boxes[0], boxes[4], boxes[8]],
-                           [boxes[2], boxes[4], boxes[6]]];
+        var allElements = [
+            [boxes[0], boxes[1], boxes[2]],
+            [boxes[3], boxes[4], boxes[5]],
+            [boxes[6], boxes[7], boxes[8]],
+            [boxes[0], boxes[3], boxes[6]],
+            [boxes[1], boxes[4], boxes[7]],
+            [boxes[2], boxes[5], boxes[8]],
+            [boxes[0], boxes[4], boxes[8]],
+            [boxes[2], boxes[4], boxes[6]]
+        ];
         for (var i = 0; i < allElements.length; i++) {
             if (allElements[i].lp_allEqualTo(1) || allElements[i].lp_allEqualTo(0)) {
                 $('table button').unbind();
-                setTimeout(function() { 
-                    
+                setTimeout(function() {
+
                     // append a message after the table
                     // display 'o' if 0 won
                     // display 'x' if 1 won
@@ -63,36 +65,38 @@ $(document).ready(function() {
         }
         return 0;
     }
-    
+
     function selectFirstBox() {
         var sel = Math.floor(Math.random() * 9);
         if (boxes[sel] === -1) {
-            boxes[sel] = 0        
+            boxes[sel] = 0
             $($boxes[sel]).text('o');
         }
     }
     selectFirstBox();
-    
+
     $('#reset').bind('click', function() {
         boxes = boxes.map(function(e) {
             return -1;
         });
-        $boxes.each(function(i,e) {
-           $(e).text('');
+        $boxes.each(function(i, e) {
+            $(e).text('');
         });
         selectFirstBox();
-        
+
         $('#result').remove();
-    
+
         $('table button').bind('click', clickbind);
     });
-    
+
     function clickbind(e) {
         if (e.target.textContent === '') {
-            var sel2 = $.map($('button'), function(e) { return [e]; }).indexOf(e.target); // get javascript array
+            var sel2 = $.map($('button'), function(e) {
+                return [e];
+            }).indexOf(e.target); // get javascript array
             boxes[sel2] = 1;
             $($boxes[sel2]).text('x');
-            
+
             if (!checkWinners()) { // if someone has't won, then let the computer choose
                 while (true) {
                     var sel3 = Math.floor(Math.random() * 9); // select random slot
@@ -106,6 +110,6 @@ $(document).ready(function() {
             }
         }
     }
-    
+
     $('table button').bind('click', clickbind);
 });
