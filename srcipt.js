677d0a9e5f8275f0f4eeb125f250842e1f1b394c
Christopher <chrisblupodev@gmail.com>
2016-06-08 13:09:27 +0000

    use array to store box model instead of object

--- srcipt.js
+++ srcipt.js
@@ -1,3 +1,101 @@
+var app = angular.module('app', ['ngMaterial'])
+.run(function($rootScope, $log) {
+    $rootScope.$log = $log;
+})
+app.controller('MainCtrl', ['$scope', function($scope) {
+    
+    var p = "num"; // box number prefix
+    var S = $scope;
+    S.boxes = lp_initfillArray(-1, 9);
+    S.box = new Array(9);
+    
+    // select first box    
+    var sel = Math.floor(Math.random() * 9);
+    l(p.concat(sel));
+    l(S.box[sel] === undefined);
+    if (S.box[sel] === undefined) {
+        S.box[sel] = "O";
+    }
+    
+}]);
+
+$(document).ready(function() {
+
+    var $boxes = $('.box');
+
+    function checkWinners() {
+        var allElements = [
+            [boxes[0], boxes[1], boxes[2]],
+            [boxes[3], boxes[4], boxes[5]],
+            [boxes[6], boxes[7], boxes[8]],
+            [boxes[0], boxes[3], boxes[6]],
+            [boxes[1], boxes[4], boxes[7]],
+            [boxes[2], boxes[5], boxes[8]],
+            [boxes[0], boxes[4], boxes[8]],
+            [boxes[2], boxes[4], boxes[6]]
+        ];
+        for (var i = 0; i < allElements.length; i++) {
+            if (allElements[i].lp_allEqualTo(1) || allElements[i].lp_allEqualTo(0)) {
+                $('.box').unbind();
+                setTimeout(function() {
+
+                    // append a message after the table
+                    // display 'o' if 0 won
+                    // display 'x' if 1 won
+                    $('main').append('<p id="result">{0}   won</p>'.lp_format(allElements[i][0] === 1 ? 'x' : 'o'));
+                }, 500);
+                return 1;
+            }
+        }
+        return 0;
+    }
+
+    function selectFirstBox() {
+    }
+    selectFirstBox();
+
+    $('#reset').bind('click', function() {
+        boxes = boxes.map(function(e) {
+            return -1;
+        });
+        $boxes.each(function(i, e) {
+            $(e).text('');
+        });
+        selectFirstBox();
+
+        $('#result').remove();
+
+        $('.box').bind('click', clickbind);
+    });
+
+    function clickbind(e) {
+        if (e.target.textContent === '') {
+            var sel2 = $.map($('button'), function(e) {
+                return [e];
+            }).indexOf(e.target); // get javascript array
+            boxes[sel2] = 1;
+            $($boxes[sel2]).text('x');
+            $($boxes[sel2]).css('color', 'black');
+
+            if (!checkWinners()) { // if someone has't won, then let the computer choose
+                while (true) {
+                    var sel3 = Math.floor(Math.random() * 9); // select random slot
+                    if (boxes[sel3] === -1) { // if the slot is blank
+                        boxes[sel3] = 0; // mark the slot zero
+                        $($boxes[sel3]).text('o'); // update the view
+                        break;
+                    }
+                }
+                checkWinners();
+            }
+        }
+    }
+
+    $('.box').bind('click', clickbind);
+});
+
+
+
 function l(message) {
     console.log(message);
 }
@@ -31,85 +129,3 @@ if (!Array.lp_allEqualTo) {
         return rtn;
     }
 }
-
-
-
-$(document).ready(function() {
-
-    var boxes = lp_initfillArray(-1, 9);
-    var $boxes = $('table button');
-
-    function checkWinners() {
-        var allElements = [
-            [boxes[0], boxes[1], boxes[2]],
-            [boxes[3], boxes[4], boxes[5]],
-            [boxes[6], boxes[7], boxes[8]],
-            [boxes[0], boxes[3], boxes[6]],
-            [boxes[1], boxes[4], boxes[7]],
-            [boxes[2], boxes[5], boxes[8]],
-            [boxes[0], boxes[4], boxes[8]],
-            [boxes[2], boxes[4], boxes[6]]
-        ];
-        for (var i = 0; i < allElements.length; i++) {
-            if (allElements[i].lp_allEqualTo(1) || allElements[i].lp_allEqualTo(0)) {
-                $('table button').unbind();
-                setTimeout(function() {
-
-                    // append a message after the table
-                    // display 'o' if 0 won
-                    // display 'x' if 1 won
-                    $($('div')).append('<span id="result">{0}   won</span>'.lp_format(allElements[i][0] === 1 ? 'x' : 'o'));
-                }, 500);
-                return 1;
-            }
-        }
-        return 0;
-    }
-
-    function selectFirstBox() {
-        var sel = Math.floor(Math.random() * 9);
-        if (boxes[sel] === -1) {
-            boxes[sel] = 0
-            $($boxes[sel]).text('o');
-        }
-    }
-    selectFirstBox();
-
-    $('#reset').bind('click', function() {
-        boxes = boxes.map(function(e) {
-            return -1;
-        });
-        $boxes.each(function(i, e) {
-            $(e).text('');
-        });
-        selectFirstBox();
-
-        $('#result').remove();
-
-        $('table button').bind('click', clickbind);
-    });
-
-    function clickbind(e) {
-        if (e.target.textContent === '') {
-            var sel2 = $.map($('button'), function(e) {
-                return [e];
-            }).indexOf(e.target); // get javascript array
-            boxes[sel2] = 1;
-            $($boxes[sel2]).text('x');
-
-            if (!checkWinners()) { // if someone has't won, then let the computer choose
-                while (true) {
-                    var sel3 = Math.floor(Math.random() * 9); // select random slot
-                    if (boxes[sel3] === -1) { // if the slot is blank
-                        boxes[sel3] = 0; // mark the slot zero
-                        $($boxes[sel3]).text('o'); // update the view
-                        break;
-                    }
-                }
-                checkWinners();
-            }
-        }
-    }
-
-    $('table button').bind('click', clickbind);
-});
